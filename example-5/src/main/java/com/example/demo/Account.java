21bcdb524c7b04fe5e1fbcc4a0c10b8301e0accc
Administrator <admin@example.com>
2017-11-01 10:42:15 +0800

    use lombok

--- example-5/src/main/java/com/example/demo/Account.java
+++ example-5/src/main/java/com/example/demo/Account.java
@@ -1,33 +1,15 @@
 package com.example.demo;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * TODO: comment here
  */
+
+@Data
+@ToString
 public class Account {
 	private String username;
 	private Integer balance;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public Integer getBalance() {
-		return balance;
-	}
-
-	public void setBalance(Integer balance) {
-		this.balance = balance;
-	}
-
-	@Override
-	public String toString() {
-		return "Account{" +
-				"username='" + username + '\'' +
-				", balance=" + balance +
-				'}';
-	}
 }
